2997c6e0a9b78f0fa77a5c9ea0aa74f2b9073a35
kalouo <omar@zipmex.com>
2021-08-18 11:29:16 +0200

    remove console.log

--- deploy/01_deploy_GSB.ts
+++ deploy/01_deploy_GSB.ts
@@ -14,7 +14,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     const decimalFactor = BigNumber.from(10).pow(18);
     const maxSupply = ethers.BigNumber.from(1_000_000).mul(decimalFactor).toString();
-    console.log(maxSupply)
+
     const {
       receipt: { contractAddress },
     } = await deploy('GSB', {
